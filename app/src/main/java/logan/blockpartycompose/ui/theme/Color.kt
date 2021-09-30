4b3973e0ee73f3fc43639102feb08eca2962b0a7
Logan Velasco <logan@bcbsil.com>
2021-09-29 17:05:00 -0700

    updates colors and themes

--- app/src/main/java/logan/blockpartycompose/ui/theme/Color.kt
+++ app/src/main/java/logan/blockpartycompose/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package logan.blockpartycompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Charcoal = Color(0xFF2F4858)
+val DarkCharcoal = Color(0xFF243742)
+val QueenBlue = Color(0xFF33658A)
+val BeuBlue = Color(0xFFB9CDDA)
